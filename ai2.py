335c26197d1dddaae6718aff8d7133f03d712e86
Rafał Łukawski <rs.lukawski@gmail.com>
2021-11-13 07:50:50 +0100

    init commit

---
+++ ai2.py
@@ -0,0 +1,46 @@
+# AI for Doom
+
+# Importing the libraries
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+# Importing the packages for OpenAI and Doom
+import gym
+#from gym.wrappers import SkipWrapper
+#from gym.wrappers.frame_skipping import SkipWrapper
+# from ppaquette_gym_doom.wrappers.action_space import ToDiscrete
+
+# Importing the other Python files
+# import experience_replay, image_preprocessing
+
+
+
+# Part 1 - Building the AI
+
+# Making the brain
+
+class CNN(nn.Module):
+    def __init__(self, number_actions):
+        super(CNN, self).__init__()
+        self.convolution1 = nn.Conv2d(in_channels = 1, out_channels = 32, kernel_size = 5)
+        self.convolution2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3)
+        self.convolution3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 2)
+        self.fc1 = nn.Linear(in_features = number_neurons, out_features = 40)
+        self.fc2 = nn.Linear(in_feature = 40, out_features = number_actions)
+        
+
+
+# Making the body
+
+
+
+# Making the AI
+
+# class AI:
+
+# Part 2 - Training the AI with Deep Convolutional Q-Learning
+
